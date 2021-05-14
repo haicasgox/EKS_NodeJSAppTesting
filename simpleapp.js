47d8e5b6c5e86cfcb561b32c57a910d0773ab2fc
Ubuntu <ubuntu@ip-172-31-38-203.us-west-1.compute.internal>
2021-05-14 08:14:15 +0000

    updated nodejs file

--- simpleapp.js
+++ simpleapp.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
 	res.writeHead(200);
-	res.write('Hello World From NodeJS Abdullah');
+	res.write('This is a simple nodejs app from K8S slayer');
 	res.end();
 });
 server.listen(8080)
